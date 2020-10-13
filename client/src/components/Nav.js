c59442e3138e96a3c26c07ebd556e555de8ab024
iden <iden@idenui-MacBook-Pro.local>
2020-10-13 17:48:23 +0900

    Fixed errors and Changed prettier settings

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -3,12 +3,11 @@ import logo from '../images/logo.svg';
 import './Nav.css';
 import { Link } from 'react-router-dom';
 
-
 function NavBar() {
   return (
-    <div className="nav-form">
-      <Link to="/">
-        <img className="nav-logo" src={logo} alt="logo"></img>
+    <div className='nav-form'>
+      <Link to='/'>
+        <img className='nav-logo' src={logo} alt='logo'></img>
       </Link>
     </div>
   );
--- client/src/components/SearchR.js
+++ client/src/components/SearchR.js
@@ -1,8 +1,8 @@
-import React, { useState } from "react";
-import SearchBar from "./SearchBar";
-import { connect } from "react-redux";
-import * as searchlist from "../modules/searchlist";
-import Pagination from "./Pagination";
+import React, { useState } from 'react';
+import SearchBar from './SearchBar';
+import { connect } from 'react-redux';
+import * as searchlist from '../modules/searchlist';
+import Pagination from './Pagination';
 
 class SearchR extends React.Component {
   constructor() {
@@ -11,7 +11,7 @@ class SearchR extends React.Component {
     // 임의 데이터 생성 (서버에 디비 작업 아직 안됨)
     let exampleItems = [...Array(150).keys()].map((i) => ({
       id: i + 1,
-      name: "변호사법 " + (i + 1),
+      name: '변호사법 ' + (i + 1),
     }));
 
     this.state = {
@@ -22,19 +22,16 @@ class SearchR extends React.Component {
   }
 
   onChangePage(pageOfItems) {
-    // update state with new page of items
+    // 데이터들의 새로운 페이지로 스테이트 업데이트
     this.setState({ pageOfItems: pageOfItems });
   }
 
   render() {
     return (
       <div>
-        <link
-          rel="stylesheet"
-          href="https://netdna.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css"
-        />
-        <div className="container">
-          <div className="text-center">
+        <div className='container'>
+          <SearchBar />
+          <div className='text-center'>
             <h1>페이지네이션</h1>
             {this.state.pageOfItems.map((item) => (
               <div key={item.id}>{item.name}</div>
@@ -48,9 +45,9 @@ class SearchR extends React.Component {
           </div>
         </div>
         <hr />
-        <div className="credits text-center">
+        <div className='credits text-center'>
           <p>
-            <a href="/">주식회사 까리용</a>
+            <a href='/'>주식회사 까리용</a>
           </p>
           <p>© 2019 Carillon Inc., All rights reserved.</p>
         </div>
