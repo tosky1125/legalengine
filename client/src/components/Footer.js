41fc3eae5cccc8c06f259a996505d1475d0c8e97
inseob jeon <pravda@pravdalaptop.local>
2020-10-18 14:18:15 +0900

    in progress making footer

---
+++ client/src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Footer.css'
+
+// 주식회사 까리용
+// © 2019 Carillon Inc., All rights reserved.
+
+// vh, rem CSS 공부하기
+
+function Footer() {
+    return (
+        <div className='foot-form'>
+            주식회사 까리용
+        </div>
+    )
+};
+
+export default Footer;
