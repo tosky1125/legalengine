427f592ee7f6d090ac0dcffb62dc0493112acf7d
inseob jeon <pravda@pravdalaptop.local>
2020-10-25 11:26:34 +0900

    modify extract method

--- strHandlerSet.js
+++ strHandlerSet.js
@@ -21,17 +21,17 @@ const extractKeyword = (str) => {
     // 먼저 법의 이름에 띄어쓰기가 되어있는 경우 띄어쓰기 기준으로 단어를 잘라준다
     const splittedStr = str.split(' ');
     // 그러나 띄어쓰기가 없는 법명이 인자로 들어와 띄어쓰기 단위로 잘리지 않았다면, keyword 에 str 를 할당하고 return 해준다
-    if (splittedStr.length === 1) {
-        keyword = splittedStr[0];
-        return keyword;
-        // 위의 경우가 아니라 일반적인 띄어쓰기 규칙이 적용된 경우라면 keyword 의 절반에 해당하는 값을 잘라준다
-    } else {
+    // if (splittedStr.length === 1) {
+    //     keyword = splittedStr[0];
+    //     return keyword;
+    //     // 위의 경우가 아니라 일반적인 띄어쓰기 규칙이 적용된 경우라면 keyword 의 절반에 해당하는 값을 잘라준다
+    // } else {
         const halfLength = Math.ceil(splittedStr.length / 2);
         const partOfsplittedStr = splittedStr.slice(0, halfLength);
         return partOfsplittedStr.reduce((accm, curr) => {
             return accm + ' ' + curr;
         });
-    }
+    // }
 };
 
 // const res = rmSpaceAndSymbols("10ㆍ27법난 피해자의 명예회복 등에 관한 법률");
