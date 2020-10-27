af6dac0b0739bd539a8d72f217748901ea3a2bed
Iden Han <idenhan129@gmail.com>
2020-10-27 22:48:44 +0900

    Make printable area within browser

--- client/src/components/ConvertToPDF.js
+++ client/src/components/ConvertToPDF.js
@@ -2,23 +2,19 @@ import React from 'react';
 import './ConvertToPDF.css';
 import MainInfo from './MainInfo';
 
-const ConvertToPDF = (props) => {
+function ConvertToPDF (props) {
   const { name, lawNum, enfDate } = props;
 
-  return (
-    <div className='pdf-btn-form'>
-      <div className='pdf-btn-wrapper'>
-        <button className='pdf-btn' onClick={() => window.print()}>
-          PDF & 프린트
-        </button>
+	return (
+		<div className='pdf-button-form'>
+			<div className='pdf-button-wrapper'>
+				<button className='pdf-button' onClick={() => window.print()}>
+					PDF & 프린트
+				</button>
       </div>
-      <MainInfo
-        name={name}
-        lawNum={lawNum}
-        enfDate={enfDate}
-      />
-    </div>
-  );
+			<MainInfo name={name} lawNum={lawNum} enfDate={enfDate} />
+      </div>
+	);
 };
 
 export default ConvertToPDF;
