921b30e4f8489efcdec840793cfada124f4fc2f0
Iden Han <idenhan129@gmail.com>
2020-10-27 15:54:22 +0900

    Fix errors of button work

--- client/src/components/ConvertToPDF.js
+++ client/src/components/ConvertToPDF.js
@@ -3,14 +3,12 @@ import './ConvertToPDF.css';
 import MainInfo from './MainInfo';
 
 const ConvertToPDF = (props) => {
-  const handlePrint = window.print();
-
   const { name, lawNum, enfDate } = props;
 
   return (
     <div className='pdf-btn-form'>
       <div className='pdf-btn-wrapper'>
-        <button className='pdf-btn' onClick={handlePrint}>
+        <button className='pdf-btn' onClick={() => window.print()}>
           PDF & 프린트
         </button>
       </div>
