4d7073f8c528fd88d8ba51d5769e2bdad16f983d
Iden Han <idenhan129@gmail.com>
2020-10-29 15:33:05 +0900

    Make section animated in Main

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import './Main.css';
 import SearchBar from './SearchBar';
-import logo from '../images/logo.svg';
+import Typist from 'react-typist';
 
 function Main() {
   return (
     <div className='main-container'>
-      <img className='main-logo' src={logo} alt='logo'></img>
+      <h1 className="main-logo">
+        <Typist>
+          <span className='main-ele'> 국내 최초 시간 기반 법령 검색 사이트 </span>
+          <Typist.Backspace count={49} delay={400} />
+          <span className='main-ele'> We are Faster </span>
+          <Typist.Backspace count={20} delay={200} />
+          <span className='main-ele'> We are Easier </span>
+          <Typist.Backspace count={20} delay={200} />
+          <span className='main-ele-logo'> LegalEngine<span className='main-ele-logo-dot'>. </span></span>
+        </Typist>
+      </h1>
       <SearchBar />
     </div>
   );
