75cbf65ca9dcf43b6ba15589baf9a00aa455499d
Iden Han <idenhan129@gmail.com>
2020-11-01 19:51:54 +0900

    Comment out ConvertToPDF

--- client/src/components/ConvertToPDF.js
+++ client/src/components/ConvertToPDF.js
@@ -3,8 +3,10 @@ import './ConvertToPDF.css';
 import MainInfo from './MainInfo';
 
 function ConvertToPDF() {
+  // innerHTML을 이용해 원하는 내용 프린트하는 함수
   function contentPrint() {
     const initBody = document.body.innerHTML;
+    // MainInfo 내용만을 담기 (SideInfo, ArticleLink 제외)
     window.onbeforeprint = () => {
       document.body.innerHTML = document.querySelector(
         '.maininfo-container',
@@ -14,6 +16,7 @@ function ConvertToPDF() {
       document.body.innerHTML = initBody;
     };
     window.print();
+    // reload 하지 않고는 버튼이 한번만 작동
     window.location.reload();
   }
 
