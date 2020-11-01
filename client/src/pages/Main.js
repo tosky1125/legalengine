1cf30d98cd9af60948dc2dcf03f5ac2d2213a88a
jun <doubleat.hj@gmail.com>
2020-11-01 15:45:55 +0900

    arrange component

--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Main.css';
 import Typist from 'react-typist';
-import SearchBar from './SearchBar';
+import SearchBar from '../components/SearchBar';
 
 function Main() {
   return (
