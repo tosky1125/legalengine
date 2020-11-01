b1d578ac1c5f54a1a9fc411dab1f2f834ebc9ee7
root <root@ip-172-26-4-72.ap-northeast-2.compute.internal>
2020-11-01 17:34:27 +0000

    move all files to server dir

--- cron/test.js
+++
@@ -1,5 +0,0 @@
-var CronJob = require('cron').CronJob;
-var job = new CronJob('35 13 * * *', function() {
-  console.log('You will see this message every second');
-}, null, true, 'Asia/Seoul');
-job.start();
