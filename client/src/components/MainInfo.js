1dfbea25642649b2e2b89648d878ec87fba81ea0
Iden Han <idenhan129@gmail.com>
2020-11-01 19:54:30 +0900

    Comment out MainInfo

--- client/src/components/MainInfo.js
+++ client/src/components/MainInfo.js
@@ -5,7 +5,7 @@ import './MainInfo.css';
 
 const MainInfo = (props) => {
   const { LawText } = props;
-
+  // 서버에서 받아온, 문자열로 출력되는 Html을 랜더링하기 위해 dangerouslySetInnerHTML 사용
   return (
     <div
       dangerouslySetInnerHTML={{
