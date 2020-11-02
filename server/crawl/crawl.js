fff74244870328a728e621d1d7e3200d5bf46605
root <root@ip-172-26-4-72.ap-northeast-2.compute.internal>
2020-11-02 02:09:59 +0000

    modify error

--- server/crawl/crawl.js
+++ server/crawl/crawl.js
@@ -63,7 +63,8 @@ const spec = async () => {
   });
   const page = await browser.newPage();
   await page.goto(url, {
-    waitUntil: 'networkidle0',
+    waitUntil: 'load',
+    timeout: 0,
   });
 
   const result = await page.evaluate(() => {
@@ -828,10 +829,10 @@ const init = async () => {
     law_id: a,
     tag: html,
   });
-  if(k === 10000) return 'hi';
+  if(k === 40000) return 'hi';
   k += 1;
   
   await init();
 }
-let k = 101;
+let k = 30001;
 init();
